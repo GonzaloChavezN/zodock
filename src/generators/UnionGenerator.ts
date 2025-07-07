c27f28d4831fe5c568417efa2109eb774b9a837f
Gonzalo Chavez <gonzalo.chavez@bci.cl>
2025-07-07 11:25:00 -0400

    refactor: improve option filtering in UnionGenerator

--- src/generators/UnionGenerator.ts
+++ src/generators/UnionGenerator.ts
@@ -1,12 +1,20 @@
-import type * as z from 'zod/v4/core';
+import * as z from 'zod/v4/core';
 import MockGenerator from '../MockGenerator';
 import type BaseGenerator from './BaseGenerator';
 
 export default class UnionGenerator<const TOptions extends z.$ZodType[], const T extends z.$ZodUnion<TOptions>> implements BaseGenerator<T> {
   public generate(schema: T) {
     const { options } = schema._zod.def;
-    const randomIndex = Math.floor(Math.random() * options.length);
-    const randomOption = options[randomIndex];
+
+    const filteredOptions = options.filter((option) => {
+      if (!(option instanceof z.$ZodUnion)) {
+        return true;
+      }
+      return option._zod.def.options.length > 0;
+    });
+
+    const randomIndex = Math.floor(Math.random() * filteredOptions.length);
+    const randomOption = filteredOptions[randomIndex];
 
     const mockGenerator = new MockGenerator(randomOption);
     return mockGenerator.generate() as z.infer<T>;
